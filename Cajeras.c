062146caed816b32535e93d63b2daba2d227f24e
JVRALPH <73514284+JVRALPH@users.noreply.github.com>
2020-12-07 13:54:17 -0600

    Add files via upload

---
+++ Cajeras.c
@@ -0,0 +1,100 @@
+
+#include <time.h>
+#include <stdio.h>
+#include <windows.h>			
+//#include "TADCola/TADColaEst.h" 
+#include "TADColaDin.h" 
+#define TIEMPO_BASE	100			//Tiempo base en ms
+#define TIEMPO_CLIENTE	2 		//Tiempo base en ms * 1
+#define TIEMPO_ATENCION	3		//Tiempo base en ms * 3
+
+int main(void)
+{
+	unsigned int tiempo = 0;
+	unsigned int cliente = 0;
+	unsigned int LlegadaCliente = 0;
+	int i,fila,NumCajas,atencion[10];
+	int goal=0;
+	elemento e;
+	//Inicializar la función rand
+	srand(time(NULL));
+	printf("Walmart\n");
+	printf("Cajas abiertas: \t");
+	scanf("%d",&NumCajas);
+	//Crear colas
+	cola cajera[NumCajas];
+	int att[NumCajas];
+	//Inicializar ambas colas
+	for(int z=0;z<NumCajas;z++)
+	{ Initialize(&cajera[z]);}
+	
+	//Ciclo infinito
+	if(NumCajas>0 && NumCajas<11)
+	{
+		for(i=0;i<NumCajas;i++)
+		{	
+			printf("Tiempo de atencion de cajera %d\t",i+1);
+			scanf("%d",&atencion[i]);
+			if(atencion[i] % 10!=0)
+			{
+				printf("El tiempo de atencion no es multiplo de 10ms\t");
+				exit(1);
+			}
+		}
+		printf("Tiempo de llegada de cada cliente\t");
+		scanf("%d",&LlegadaCliente);
+		if(LlegadaCliente % 10!=0)
+		{
+			printf("El tiempo de llegada no es multiplo de 10ms\t");
+			exit(1);
+		}
+	}
+	while(1)
+	{
+		Sleep(TIEMPO_BASE);		
+		tiempo++;				
+		int c=0;
+		//Si el tiempo es multiplo del tiempo de atencion
+		for(int n=0;n<NumCajas;n++)
+		{
+				if (!Empty(&cajera[n]) && tiempo%atencion[n] == 0)
+				{
+					Sleep(TIEMPO_BASE);
+					e = Dequeue(&cajera[n]);
+					printf("\n\n\nAtendi a: %d en caja %d", e.n,n);
+				}
+		}
+		//Si el tiempo es multiplo del de llegada de los clientes
+		if (tiempo % LlegadaCliente == 0)
+		{
+			goal=0;
+			cliente++;				//Incrementar el numero de clientes
+			e.n = cliente;			//Dar el numero que identifica al cliente
+			fila=rand()%NumCajas;			//Escoger la fila para formarse aleatoriamente					
+			Queue(&cajera[fila], e);//Formarse en la fila seleccionada
+			printf("\n\n\nLlego el cliente: %d a la cola de la caja %d", e.n,fila);
+			goal+=e.n;
+		}
+		if(goal>=100)
+		{
+			for(int goal1=0;goal1<NumCajas;goal1++)
+			{
+				if (Empty(&cajera[goal1]))
+				{
+					printf("\n\n\n Ya no hay clientes formados, la tienda ha cerrado");
+					exit(1);
+				}
+			}
+		}
+		/*/Mostrar los clientes en cada cola
+		printf("\n\n%d clientes en cola 0: [",Size(&cajera[0]));
+		for (i=1;i<=Size(&cajera[0]);i++)
+		{
+			e=Element(&cajera[0],i);
+			printf("%d\t", e.n);
+		}
+		printf("]");*/
+		c++;
+	}
+	return 0;
+}
